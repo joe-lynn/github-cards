fbd36249f9d7aa5c4e7255fd5f817edff05c15f6
joe-lynn <jml1074@rit.edu>
2018-06-08 10:46:30 -0400

    added github public user api get

---
+++ githubapi.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    };
+    xhttp.open("GET", "https://api.github.com/users/joe-lynn", true);
+    xhttp.send();
+});
+
+// Response:
+// {
+//     "login": "joe-lynn",
+//     "id": 18252709,
+//     "node_id": "MDQ6VXNlcjE4MjUyNzA5",
+//     "avatar_url": "https://avatars2.githubusercontent.com/u/18252709?v=4",
+//     "gravatar_id": "",
+//     "url": "https://api.github.com/users/joe-lynn",
+//     "html_url": "https://github.com/joe-lynn",
+//     "followers_url": "https://api.github.com/users/joe-lynn/followers",
+//     "following_url": "https://api.github.com/users/joe-lynn/following{/other_user}",
+//     "gists_url": "https://api.github.com/users/joe-lynn/gists{/gist_id}",
+//     "starred_url": "https://api.github.com/users/joe-lynn/starred{/owner}{/repo}",
+//     "subscriptions_url": "https://api.github.com/users/joe-lynn/subscriptions",
+//     "organizations_url": "https://api.github.com/users/joe-lynn/orgs",
+//     "repos_url": "https://api.github.com/users/joe-lynn/repos",
+//     "events_url": "https://api.github.com/users/joe-lynn/events{/privacy}",
+//     "received_events_url": "https://api.github.com/users/joe-lynn/received_events",
+//     "type": "User",
+//     "site_admin": false,
+//     "name": null,
+//     "company": null,
+//     "blog": "",
+//     "location": "United States",
+//     "email": null,
+//     "hireable": true,
+//     "bio": "Software Engineering Student at Rochester Institute Of Technology",
+//     "public_repos": 14,
+//     "public_gists": 0,
+//     "followers": 3,
+//     "following": 5,
+//     "created_at": "2016-04-04T00:57:43Z",
+//     "updated_at": "2018-06-06T17:30:42Z"
+// }
